3d97550ef5f0c3be854ee7cc833f6f88acbd335b
Sean Marshallsay <srm.1708@gmail.com>
2015-05-25 13:28:26 +0100

    More correct setup.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-import hippy
+import os
 from setuptools import setup
 
+base = os.path.dirname(__file__)
+
+mdata = {}
+with open(os.path.join(base, 'hippy', '__about__.py')) as f:
+    exec(f.read(), mdata)
+
 setup(
-    name=hippy.__title__,
-    version=hippy.__version__,
-    author=hippy.__author__,
-    author_email=hippy.__email__,
-    description=hippy.__description__,
-    long_description=open('README.rst').read(),
-    url=hippy.__homepage__,
-    download_url=hippy.__download__,
+    name=mdata['__title__'],
+    version=mdata['__version__'],
+    author=mdata['__author__'],
+    author_email=mdata['__email__'],
+    description=mdata['__description__'],
+    long_description=open(os.path.join('README.rst')).read(),
+    url=mdata['__homepage__'],
+    download_url=mdata['__download__'],
     packages=['hippy'],
 )
